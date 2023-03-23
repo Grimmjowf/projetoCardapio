2475cf5f08584d356032ee2b4588353494772942
Jonathan de Almeida da Silva <98177025+Grimmjowf@users.noreply.github.com>
2023-03-23 15:19:53 -0300

    1
    Teste para subir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'produtos',
+    loadChildren: () => import('./produtos/produtos.module').then( m => m.ProdutosPageModule)
   },
+
 ];
 
 @NgModule({
