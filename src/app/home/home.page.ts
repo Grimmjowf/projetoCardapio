ab1618908ae1a04a79662797e5f9066e04f5aebe
Jonathan de Almeida da Silva <ajonathan967@gmail.com>
2023-04-02 10:58:11 -0300

    ngFor e imagens
    implementação do ngFor e imagens

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,8 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  public segment = '0';
+  public burguer=[
+    {nome:'Bull Ticken',categoria:1, descricao:'', valor:32.90, imagem:'01.bull.ticken.png'},
+    {nome:'',categoria:'', descricao:'', valor:34.90, imagem:'02.bull.duplo'},
+    {nome:'',categoria:'', descricao:'', valor:''},
+    {nome:'',categoria:'', descricao:'', valor:''},
+    {nome:'',categoria:'', descricao:'', valor:''},
+  ]
+  public drink=[
+    {nome:'',categoria:'', descricao:'', valor:''},
+    {nome:'',categoria:'', descricao:'', valor:''},
+    {nome:'',categoria:'', descricao:'', valor:''},
+    {nome:'',categoria:'', descricao:'', valor:''},
+    {nome:'',categoria:'', descricao:'', valor:''},
+  ]
   
+  public burguerImg = '../../assets/imagens/';
+ 
+  public segment = '0';
+
   constructor() {}
 
 }
