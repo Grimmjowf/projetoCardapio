4421f57299e2e3549d36922d0f0e334d8b2acccb
Jonathan de Almeida da Silva <98177025+Grimmjowf@users.noreply.github.com>
2023-04-02 23:20:52 -0300

    Criação de detalhes
    Criação de e correção de página

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,11 +16,11 @@ export class HomePage {
   ]
 
   public drinks=[
-    {nome:'Caipirinha',categoria:1, descricao:'Copo de 400ml da melhor caipirinha feita pela casa.', valor:'R$ 15.00', imagem:'1.1.caipirinha.png', visibled: false},
-    {nome:'Budzinha',categoria:2, descricao:'Cerveja de garrafa Budweiser 350 ml.', valor:'R$ 8.00', imagem:'1.2.budzinha.png', visibled: false},
-    {nome:'Whisky 12 anos dose',categoria:3, descricao:'Dose de Whisky 12 anos importado diretamente da alemanha, para apreciadores de uma boa bebida.', valor:'R$ 12.00', imagem:'1.3.whisky-12-anos.png', visibled: false},
-    {nome:'Cerveja Artezanal',categoria:4, descricao:'Cerveja da casa, feita artesanalmente com um sabor delicioso para aconpanhar o seu Burguer. ', valor:'R$ 8.00', imagem:'1.4.cerveja-artesanal.png', visibled: false},
-    {nome:'Coca-cola',categoria:5, descricao:'Coca cola de 600 ml.', valor:'R$ 10.00', imagem:'1.5.coca-cola.png', visibled: false} 
+    {nome:'Caipirinha',categoria:5, descricao:'Copo de 400ml da melhor caipirinha feita pela casa.', valor:'R$ 15.00', imagem:'1.1.caipirinha.png', visibled: false},
+    {nome:'Budzinha',categoria:6, descricao:'Cerveja de garrafa Budweiser 350 ml.', valor:'R$ 8.00', imagem:'1.2.budzinha.png', visibled: false},
+    {nome:'Whisky 12 anos dose',categoria:7, descricao:'Dose de Whisky 12 anos importado diretamente da alemanha, para apreciadores de uma boa bebida.', valor:'R$ 12.00', imagem:'1.3.whisky-12-anos.png', visibled: false},
+    {nome:'Cerveja Artezanal',categoria:8, descricao:'Cerveja da casa, feita artesanalmente com um sabor delicioso para aconpanhar o seu Burguer. ', valor:'R$ 8.00', imagem:'1.4.cerveja-artesanal.png', visibled: false},
+    {nome:'Coca-cola',categoria:9, descricao:'Coca cola de 600 ml.', valor:'R$ 10.00', imagem:'1.5.coca-cola.png', visibled: false} 
   ]
   
   public produtosImg = '../../assets/imagens/';
